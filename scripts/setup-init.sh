c063af049d08d347e089fe18ed59f5c3504232e9
yiluohan1234 <1097189275@qq.com>
2021-09-14 15:16:08 +0800

    add setup-es

--- scripts/setup-init.sh
+++ scripts/setup-init.sh
@@ -29,3 +29,10 @@ systemctl restart sshd.service
 [ ! -d $DOWNLOAD_PATH ] && mkdir -p $DOWNLOAD_PATH
 chown -R vagrant:vagrant $INSTALL_PATH
 chown -R vagrant:vagrant $DOWNLOAD_PATH
+
+# 启动elasticsearch需要的设置
+# 更改最大文件句柄数和最大线程数限制
+echo -e "* soft nofile 65536\n* hard nofile 65536\n* soft nproc 2048\n* hard nproc 65536" >> /etc/security/limits.conf
+
+# 虚拟内存扩容
+echo "vm.max_map_count=262144" >> /etc/sysctl.conf
