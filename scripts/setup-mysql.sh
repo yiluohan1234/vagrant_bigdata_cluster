9d16938efdc2bd65ffd7fe00756374ae912d086f
yiluohan1234 <1097189275@qq.com>
2021-09-18 17:00:16 +0800

    update readme

--- scripts/setup-mysql.sh
+++ scripts/setup-mysql.sh
@@ -17,6 +17,11 @@ install_mysql()
     PORT="3306"
     USERNAME="root"
     PASSWORD="199037"
-    #mysql -uroot -p199037 -e "create user 'hive'@'%' IDENTIFIED BY 'hive';GRANT ALL PRIVILEGES ON *.* TO 'hive'@'%' WITH GRANT OPTION;grant all on *.* to 'hive'@'localhost' identified by 'hive';flush privileges; quit"
+    # 创建hive的元数据库
+    #mysql -uroot -p199037 -e "create user 'hive'@'%' IDENTIFIED BY 'hive';GRANT ALL PRIVILEGES ON *.* TO 'hive'@'%' WITH GRANT OPTION;grant all on *.* to 'hive'@'localhost' identified by 'hive';flush privileges;"
+
+    # 进行远程访问授权
+    #mysql -uroot -p199037 -e "use mysql; update user set authentication_string=password('199037') where user='root'; update user set authentication_string=password('199037'),plugin='mysql_native_password' where user='root';grant all on *.* to root@'%' identified by '199037' with grant option;grant all privileges on *.* to 'root'@'%' identified by '199037' with grant option;flush privileges;"
+
 }
 install_mysql
