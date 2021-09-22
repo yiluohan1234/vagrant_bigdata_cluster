2c55dc8d8365b157ec6b1ff55963dbf2ed494a8d
yiluohan1234 <1097189275@qq.com>
2021-09-22 16:01:26 +0800

    update download repos

--- scripts/common.sh
+++ scripts/common.sh
@@ -3,7 +3,7 @@
 RESOURCE_PATH=/home/vagrant/resources
 
 # 安装目录
-INSTALL_PATH=/home/vagrant/apps
+INSTALL_PATH=/home/vagrant/.apps
 
 # 组件下载目录
 DOWNLOAD_PATH=/home/vagrant/downloads
@@ -18,6 +18,10 @@ IS_VAGRANT="true"
 # 环境变量配置文件
 PROFILE=~/.bashrc
 
+# 下载组建的镜像地址
+# 1:https://archive.apache.org/dist
+# 2:https://mirrors.huaweicloud.com/apache
+DOWNLOAD_REPO=https://mirrors.huaweicloud.com/apache
 # hostname
 HOSTNAME=("hdp101" "hdp102" "hdp103")
 
@@ -25,12 +29,12 @@ HOSTNAME=("hdp101" "hdp102" "hdp103")
 SSH_CONF=/home/vagrant/resources/ssh
 
 # app版本
-HADOOP_VERSION=hadoop-2.7.2
+HADOOP_VERSION=hadoop-2.7.6
 HIVE_VERSION=hive-2.3.4
-HBASE_VERSION=hbase-1.2.5
+HBASE_VERSION=hbase-1.2.6
 SPARK_VERSION=spark-2.4.6
 FLINK_VERSION=flink-1.12.4
-SQOOP_VERSION=sqoop-1.4.6
+SQOOP_VERSION=sqoop-1.4.7
 ZOOKEEPER_VERSION=zookeeper-3.4.10
 KAFKA_VERSION=kafka_2.11-0.11.0.3
 FLUME_VERSION=flume-1.6.0
@@ -38,7 +42,7 @@ SCALA_VERSION=scala-2.11.12
 MAVEN_VERSION=apache-maven-3.2.5
 MYSQL_CONNECTOR_VERSION=mysql-connector-java-5.1.49
 MYSQL_VERSION=mysql-5.7.30
-PHOENIX_VERSION=apache-phoenix-4.8.1-HBase-1.2-bin
+PHOENIX_VERSION=apache-phoenix-4.14.0-HBase-1.2-bin.tar.gz
 NGINX_VERSION=nginx-1.18.0
 ES_VERSION=elasticsearch-7.6.0
 KIBANA_VERSION=kibana-7.6.0
@@ -62,76 +66,116 @@ JAVA_MIRROR_DOWNLOAD=https://repo.huaweicloud.com/java/jdk/8u201-b09/$JAVA_ARCHI
 
 # hadoop
 # 支持版本：3.3.1, 3.3.0, 3.2.2-3.2.0, 3.1.4-3.1.0, 3.0.3-3.0.0, 2.9.2-2.9.0, 2.8.5-2.8.0, 2.7.7-2.7.0等
+#         https://archive.apache.org/dist/hadoop/core/hadoop-2.7.6/hadoop-2.7.6.tar.gz
+# https://mirrors.huaweicloud.com/apache/hadoop/core/hadoop-2.7.6/hadoop-2.7.6.tar.gz
+# https://archive.apache.org/dist => https://mirrors.huaweicloud.com/apache
 HADOOP_VERSION_NUM=`get_app_version_num $HADOOP_VERSION '-' 2`
 HADOOP_ARCHIVE=$HADOOP_VERSION.tar.gz
-HADOOP_MIRROR_DOWNLOAD=http://archive.apache.org/dist/hadoop/core/$HADOOP_VERSION/$HADOOP_ARCHIVE
+HADOOP_MIRROR_DOWNLOAD=$DOWNLOAD_REPO/hadoop/core/$HADOOP_VERSION/$HADOOP_ARCHIVE
 HADOOP_RES_DIR=$RESOURCE_PATH/hadoop
 HADOOP_PREFIX=$INSTALL_PATH/hadoop
 HADOOP_CONF_DIR=$HADOOP_PREFIX/etc/hadoop
 
 # hive
 # 支持版本：3.1.2-3.1.0, 3.0.0, 2.3.9,-2.3.0, 2.2.0, 2.1.1, 2.1.0, 2.0.1, 2.0.0等
+#         https://archive.apache.org/dist/hive/hive-2.3.4/apache-hive-2.3.4-bin.tar.gz
+# https://mirrors.huaweicloud.com/apache/hive/hive-2.3.4/apache-hive-2.3.4-bin.tar.gz
 HIVE_VERSION_NUM=`get_app_version_num $HIVE_VERSION '-' 2`
 HIVE_ARCHIVE=apache-$HIVE_VERSION-bin.tar.gz
-HIVE_MIRROR_DOWNLOAD=http://archive.apache.org/dist/hive/$HIVE_VERSION/$HIVE_ARCHIVE
+HIVE_MIRROR_DOWNLOAD=$DOWNLOAD_REPO/hive/$HIVE_VERSION/$HIVE_ARCHIVE
 HIVE_RES_DIR=$RESOURCE_PATH/hive
 HIVE_CONF_DIR=$INSTALL_PATH/hive/conf
 
 # hbase
 # 支持版本：2.4.5-2.4.0, 2.3.6-2.3.0, 2.2.7-2.2.0, 2.1.10-2.1.0, 2.0.6-2.0.0等
+#         https://archive.apache.org/dist/hbase/1.2.6/hbase-1.2.6-bin.tar.gz
+# https://mirrors.huaweicloud.com/apache/hbase/1.2.6/hbase-1.2.6-bin.tar.gz
 HBASE_VERSION_NUM=`get_app_version_num $HBASE_VERSION '-' 2`
 HBASE_ARCHIVE=${HBASE_VERSION}-bin.tar.gz
-HBASE_MIRROR_DOWNLOAD=http://archive.apache.org/dist/hbase/$HBASE_VERSION_NUM/$HBASE_ARCHIVE
+HBASE_MIRROR_DOWNLOAD=$DOWNLOAD_REPO/hbase/$HBASE_VERSION_NUM/$HBASE_ARCHIVE
 HBASE_RES_DIR=$RESOURCE_PATH/hbase
 HBASE_CONF_DIR=$INSTALL_PATH/hbase/conf
 
 # spark
 # 支持版本：具体见下载地址
+#         https://archive.apache.org/dist/spark/spark-2.4.6/spark-2.4.6-bin-hadoop2.7.tgz
+# https://mirrors.huaweicloud.com/apache/spark/spark-2.4.6/spark-2.4.6-bin-hadoop2.7.tgz
 SPARK_VERSION_NUM=`get_app_version_num $SPARK_VERSION '-' 2`
 SPARK_ARCHIVE=$SPARK_VERSION-bin-hadoop2.7.tgz
-SPARK_MIRROR_DOWNLOAD=http://archive.apache.org/dist/spark/$SPARK_VERSION/$SPARK_ARCHIVE
+SPARK_MIRROR_DOWNLOAD=$DOWNLOAD_REPO/spark/$SPARK_VERSION/$SPARK_ARCHIVE
 SPARK_RES_DIR=$RESOURCE_PATH/spark
 SPARK_CONF_DIR=$INSTALL_PATH/spark/conf
 
 # flink
 # 支持版本：具体见下载地址
+#         https://archive.apache.org/dist/flink/flink-1.12.4/flink-1.12.4-bin-scala_2.11.tgz
+# https://mirrors.huaweicloud.com/apache/flink/flink-1.12.4/flink-1.12.4-bin-scala_2.11.tgz
 FLINK_VERSION_NUM=`get_app_version_num $FLINK_VERSION '-' 2`
 FLINK_ARCHIVE=$FLINK_VERSION-bin-scala_2.11.tgz
-FLINK_MIRROR_DOWNLOAD=https://archive.apache.org/dist/flink/$FLINK_VERSION/$FLINK_VERSION-bin-scala_2.11.tgz
+FLINK_MIRROR_DOWNLOAD=$DOWNLOAD_REPO/flink/$FLINK_VERSION/$FLINK_VERSION-bin-scala_2.11.tgz
 FLINK_RES_DIR=$RESOURCE_PATH/flink
 FLINK_CONF_DIR=$INSTALL_PATH/flink/conf
 
 # sqoop
 # 支持版本：1.99.7-1.99.1, 1.4.7-1.4.2（版本和下载地址要对应）
+#         https://archive.apache.org/dist/sqoop/1.4.7/sqoop-1.4.7.bin__hadoop-2.6.0.tar.gz
+# https://mirrors.huaweicloud.com/apache/sqoop/1.4.7/sqoop-1.4.7.bin__hadoop-2.6.0.tar.gz
 SQOOP_VERSION_NUM=`get_app_version_num $SQOOP_VERSION '-' 2`
-SQOOP_ARCHIVE=${SQOOP_VERSION}.bin__hadoop-2.0.4-alpha.tar.gz
-SQOOP_MIRROR_DOWNLOAD=http://archive.apache.org/dist/sqoop/$SQOOP_VERSION_NUM/$SQOOP_ARCHIVE
+SQOOP_ARCHIVE=${SQOOP_VERSION}.bin__hadoop-2.6.0.tar.gz
+SQOOP_MIRROR_DOWNLOAD=$DOWNLOAD_REPO/sqoop/$SQOOP_VERSION_NUM/$SQOOP_ARCHIVE
 SQOOP_RES_DIR=$RESOURCE_PATH/sqoop
 SQOOP_CONF_DIR=$INSTALL_PATH/sqoop/conf
 
 # zookeeper
 # 支持版本：3.7.0, 3.6.3-3.6.0, 3.5.9-3.5.5, 3.4.14-3.4.0, 3.3.6-3.3.3
+#         https://archive.apache.org/dist/zookeeper/zookeeper-3.4.10/zookeeper-3.4.10.tar.gz
+# https://mirrors.huaweicloud.com/apache/zookeeper/zookeeper-3.4.10/zookeeper-3.4.10.tar.gz
 ZOOKEEPER_VERSION_NUM=`get_app_version_num $ZOOKEEPER_VERSION '-' 2`
 ZOOKEEPER_ARCHIVE=${ZOOKEEPER_VERSION}.tar.gz
-ZOOKEEPER_MIRROR_DOWNLOAD=http://archive.apache.org/dist/zookeeper/$ZOOKEEPER_VERSION/$ZOOKEEPER_ARCHIVE
+ZOOKEEPER_MIRROR_DOWNLOAD=$DOWNLOAD_REPO/zookeeper/$ZOOKEEPER_VERSION/$ZOOKEEPER_ARCHIVE
 ZOOKEEPER_RES_DIR=$RESOURCE_PATH/zookeeper
 ZOOKEEPER_CONF_DIR=$INSTALL_PATH/zookeeper/conf
 
 # kafka
 # 支持版本：具体查看下载地址
+#         https://archive.apache.org/dist/kafka/0.11.0.3/kafka_2.11-0.11.0.3.tgz
+# https://mirrors.huaweicloud.com/apache/kafka/0.11.0.3/kafka_2.11-0.11.0.3.tgz
 KAFKA_VERSION_NUM=`get_app_version_num $KAFKA_VERSION '-' 2`
 KAFKA_ARCHIVE=${KAFKA_VERSION}.tgz
-KAFKA_MIRROR_DOWNLOAD=https://archive.apache.org/dist/kafka/$KAFKA_VERSION_NUM/$KAFKA_ARCHIVE
+KAFKA_MIRROR_DOWNLOAD=$DOWNLOAD_REPO/kafka/$KAFKA_VERSION_NUM/$KAFKA_ARCHIVE
 KAFKA_RES_DIR=$RESOURCE_PATH/kafka
 KAFKA_CONF_DIR=$INSTALL_PATH/kafka/config
 
 # flume
 # 支持版本：1.9.0, 1.8.0, 1.7.0, 1.6.0, 1.5.2-1.5.0等
+#        https://archive.apache.org/dist/flume/1.6.0/apache-flume-1.6.0-bin.tar.gz
+# https://mirrors.huaweicloud.com/apache/flume/1.6.0/apache-flume-1.6.0-bin.tar.gz
 FLUME_VERSION_NUM=`get_app_version_num $FLUME_VERSION '-' 2`
 FLUME_ARCHIVE=apache-${FLUME_VERSION}-bin.tar.gz
-FLUME_MIRROR_DOWNLOAD=https://archive.apache.org/dist/flume/$FLUME_VERSION_NUM/$FLUME_ARCHIVE
+FLUME_MIRROR_DOWNLOAD=$DOWNLOAD_REPO/flume/$FLUME_VERSION_NUM/$FLUME_ARCHIVE
 FLUME_RES_DIR=$RESOURCE_PATH/flume
 FLUME_CONF_DIR=$INSTALL_PATH/flume/conf
+
+# maven
+# 支持版本：具体见下载地址
+# 注意：Maven 3.3.x 可以构建 Flink，但是不能正确地屏蔽掉指定的依赖。Maven 3.2.5 可以正确地构建库文件
+#        https://archive.apache.org/dist/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz
+# https://mirrors.huaweicloud.com/apache/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz
+MAVEN_VERSION_NUM=`get_app_version_num $MAVEN_VERSION '-' 3`
+MAVEN_ARCHIVE=${MAVEN_VERSION}-bin.tar.gz
+MAVEN_MIRROR_DOWNLOAD=$DOWNLOAD_REPO/maven/maven-${MAVEN_VERSION_NUM%%.*}/$MAVEN_VERSION_NUM/binaries/$MAVEN_ARCHIVE
+MAVEN_RES_DIR=$RESOURCE_PATH/maven
+MAVEN_CONF_DIR=$INSTALL_PATH/maven/conf
+
+# phoenix
+# 支持版本：具体查看下载地址
+#        https://archive.apache.org/dist/phoenix/apache-phoenix-4.14.0-HBase-1.2/bin/apache-phoenix-4.14.0-HBase-1.2-bin.tar.gz
+# https://mirrors.huaweicloud.com/apache/phoenix/apache-phoenix-4.14.1-HBase-1.2/bin/apache-phoenix-4.14.1-HBase-1.2-bin.tar.gz
+PHOENIX_VERSION_NUM=`get_app_version_num $PHOENIX_VERSION '-' 3`
+PHOENIX_ARCHIVE=${PHOENIX_VERSION}.tar.gz
+PHOENIX_MIRROR_DOWNLOAD=$DOWNLOAD_REPO/phoenix/apache-phoenix-4.14.0-HBase-1.2/bin/$PHOENIX_ARCHIVE
+PHOENIX_RES_DIR=$RESOURCE_PATH/phoenix
+PHOENIX_CONF_DIR=$INSTALL_PATH/phoenix/conf
 
 # scala
 # 支持版本：2.10.X, 2.11.X, 2.12.X, 2.13.X
@@ -140,20 +184,13 @@ SCALA_ARCHIVE=${SCALA_VERSION}.tgz
 # SCALA_MIRROR_DOWNLOAD=https://downloads.lightbend.com/scala/2.11.12/scala-2.11.12.tgz 
 SCALA_MIRROR_DOWNLOAD=https://distfiles.macports.org/scala${SCALA_VERSION_NUM%.*}/$SCALA_ARCHIVE
 
-# maven
-# 支持版本：具体见下载地址
-# 注意：Maven 3.3.x 可以构建 Flink，但是不能正确地屏蔽掉指定的依赖。Maven 3.2.5 可以正确地构建库文件
-MAVEN_VERSION_NUM=`get_app_version_num $MAVEN_VERSION '-' 3`
-MAVEN_ARCHIVE=${MAVEN_VERSION}-bin.tar.gz
-MAVEN_MIRROR_DOWNLOAD=https://archive.apache.org/dist/maven/maven-${MAVEN_VERSION_NUM%%.*}/$MAVEN_VERSION_NUM/binaries/$MAVEN_ARCHIVE
-MAVEN_RES_DIR=$RESOURCE_PATH/maven
-MAVEN_CONF_DIR=$INSTALL_PATH/maven/conf
-
 # mysql_connector
 # 支持版本：具体见下载地址
+#                 http://mirrors.sohu.com/mysql/Connector-J/mysql-connector-java-5.1.49.tar.gz
+# https://repo.huaweicloud.com/mysql/Downloads/Connector-J/mysql-connector-java-5.1.49.tar.gz
 MYSQL_CONNECTOR_VERSION_NUM=`get_app_version_num $MYSQL_CONNECTOR_VERSION '-' 4`
 MYSQL_CONNECTOR_ARCHIVE=${MYSQL_CONNECTOR_VERSION}.tar.gz
-MYSQL_CONNECTOR_MIRROR_DOWNLOAD=http://mirrors.sohu.com/mysql/Connector-J/$MYSQL_CONNECTOR_ARCHIVE
+MYSQL_CONNECTOR_MIRROR_DOWNLOAD=https://repo.huaweicloud.com/mysql/Downloads/Connector-J/$MYSQL_CONNECTOR_ARCHIVE
 
 # mysql
 # 支持版本：具体见下载地址
@@ -162,19 +199,13 @@ MYSQL_ARCHIVE=${MYSQL_VERSION}-linux-glibc2.12-x86_64.tar.gz
 MYSQL_MIRROR_DOWNLOAD=https://cdn.mysql.com/archives/mysql-${MYSQL_VERSION_NUM%.*}/$MYSQL_VERSION-linux-glibc2.12-x86_64.tar.gz
 MYSQL_RES_DIR=$RESOURCE_PATH/mysql
 
-# phoenix
-# 支持版本：具体查看下载地址
-PHOENIX_VERSION_NUM=`get_app_version_num $PHOENIX_VERSION '-' 3`
-PHOENIX_ARCHIVE=${PHOENIX_VERSION}.tar.gz
-PHOENIX_MIRROR_DOWNLOAD=https://archive.apache.org/dist/phoenix/apache-phoenix-4.8.1-HBase-1.2/bin/$PHOENIX_ARCHIVE
-PHOENIX_RES_DIR=$RESOURCE_PATH/phoenix
-PHOENIX_CONF_DIR=$INSTALL_PATH/phoenix/conf
-
 # nginx
 # 支持版本：具体见下载地址
+# https://nginx.org/download/nginx-1.18.0.tar.gz
+# https://repo.huaweicloud.com/nginx/nginx-1.18.0.tar.gz
 NGINX_VERSION_NUM=`get_app_version_num $NGINX_VERSION '-' 2`
 NGINX_ARCHIVE=${NGINX_VERSION}.tar.gz
-NGINX_MIRROR_DOWNLOAD=http://nginx.org/download/$NGINX_ARCHIVE
+NGINX_MIRROR_DOWNLOAD=https://repo.huaweicloud.com/nginx/$NGINX_ARCHIVE
 NGINX_RES_DIR=$RESOURCE_PATH/nginx
 NGINX_CONF_DIR=$INSTALL_PATH/nginx/conf
 
@@ -200,6 +231,7 @@ KIBANA_CONF_DIR=$INSTALL_PATH/kibana/config
 # redis
 # 支持版本：具体见下载地址
 # https://mirrors.huaweicloud.com/redis/redis-6.2.1.tar.gz
+# https://repo.huaweicloud.com/redis/redis-6.2.1.tar.gz
 REDIS_VERSION_NUM=`get_app_version_num $REDIS_VERSION '-' 2`
 REDIS_ARCHIVE=$REDIS_VERSION.tar.gz
 REDIS_MIRROR_DOWNLOAD=https://mirrors.huaweicloud.com/redis/$REDIS_ARCHIVE
--- scripts/setup-hadoop.sh
+++ scripts/setup-hadoop.sh
@@ -93,7 +93,7 @@ install_hadoop() {
     setup_hadoop $app_name
     setupEnv_app $app_name sbin
     # 解决Unable to load native-hadoop library for your platform
-    echo "export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native/:$LD_LIBRARY_PATH" >> $PROFILE
+    echo 'export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native/:$LD_LIBRARY_PATH' >> $PROFILE
     #dispatch_app $app_name
     if [ "$IS_VAGRANT" != "true" ];then
         dispatch_app $app_name
