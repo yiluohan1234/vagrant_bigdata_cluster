cc2b2d2b0ae40a6f7e9275356ae4bd811d4dc22f
yiluohan1234 <1097189275@qq.com>
2021-09-23 11:05:24 +0800

    update setup-redis.sh

--- scripts/setup-redis.sh
+++ scripts/setup-redis.sh
@@ -24,9 +24,9 @@ setup_redis() {
     #bind 127.0.0.1 -::1
     sed -i 's@^bind 127.0.0.1 -::1*@#bind 127.0.0.1 -::1@' $REDIS_CONF_DIR/redis.conf
     sed -i 's@^daemonize no*@daemonize yes@' $REDIS_CONF_DIR/redis.conf
-    #sed -i 's@^pidfile /var/run/redis_6379.pid*@pidfile '$INSTALL_PATH'/redis/run/redis_6379.pid@' $REDIS_CONF_DIR/redis.conf
-    #sed -i 's@^logfile ""*@logfile "'$INSTALL_PATH'/redis/logs"@' $REDIS_CONF_DIR/redis.conf
-    #sed -i 's@^dir ./*@dir '$INSTALL_PATH'/redis/data@' $REDIS_CONF_DIR/redis.conf
+    sed -i 's@^pidfile /var/run/redis_6379.pid*@pidfile '$INSTALL_PATH'/redis/run/redis_6379.pid@' $REDIS_CONF_DIR/redis.conf
+    sed -i 's@^logfile ""*@logfile "'$INSTALL_PATH'/redis/logs"@' $REDIS_CONF_DIR/redis.conf
+    sed -i 's@^dir ./*@dir '$INSTALL_PATH'/redis/data@' $REDIS_CONF_DIR/redis.conf
     sed -i 's@protected-mode yes@protected-mode no@' $REDIS_CONF_DIR/redis.conf
     
     if [ $INSTALL_PATH != /home/vagrant/apps ];then
