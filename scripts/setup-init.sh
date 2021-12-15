58c6915f073598e7e333894fbd7869341e115f21
yiluohan1234 <1097189275@qq.com>
2021-12-15 11:07:48 +0800

    update default git version

--- scripts/setup-init.sh
+++ scripts/setup-init.sh
@@ -23,7 +23,8 @@ yum install -y openssl-devel
 yum install -y lzop
 
 # git升级
-yum -y install https://packages.endpoint.com/rhel/7/os/x86_64/endpoint-repo-1.7-1.x86_64.rpm
+yum remove -y git*
+yum install -y https://packages.endpoint.com/rhel/7/os/x86_64/endpoint-repo-1.7-1.x86_64.rpm
 yum install -y git
 
 #---ssh---
