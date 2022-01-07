5779b788dffce09897afc285abf4776f2a470c24
yiluohan1234 <1097189275@qq.com>
2022-01-08 00:02:37 +0800

    update setup-mysql

--- scripts/setup-mysql.sh
+++ scripts/setup-mysql.sh
@@ -15,14 +15,14 @@ setup_mysql() {
 
     # 安装依赖
     yum install -y libaio
-    log info "install ${app_name}"
-    log info "初始化：建立install_dir、data_dir和用户群组"
+    log info "下载安装mysql"
+    # 初始化：建立install_dir、data_dir和用户群组
     id -u mysql >/dev/null 2>&1
     [ $? -ne 0 ] && useradd -M -s /sbin/nologin mysql
     [ ! -d "${mysql_install_dir}" ] && mkdir -p ${mysql_install_dir}
     mkdir -p ${mysql_data_dir};chown mysql.mysql -R ${mysql_data_dir}
 
-    log info "下载解压mysql"
+    # 下载解压mysql
     if resourceExists ${MYSQL_ARCHIVE}; then
         tar -xzf ${DOWNLOAD_PATH}/${MYSQL_ARCHIVE} -C ${DOWNLOAD_PATH}
     else
@@ -32,32 +32,33 @@ setup_mysql() {
     mv ${DOWNLOAD_PATH}/${MYSQL_VERSION}-linux-glibc2.12-x86_64/* ${mysql_install_dir}
     rm -rf ${DOWNLOAD_PATH}/${MYSQL_ARCHIVE}*
 
-    log info "拷贝配置文件my.cnf"
+    # 拷贝配置文件my.cnf
     cp ${MYSQL_RES_DIR}/my.cnf /etc/
 
     #sed -i 's@executing mysqld_safe@executing mysqld_safe\nexport LD_PRELOAD=/usr/local/lib/libjemalloc.so@' ${mysql_install_dir}/bin/mysqld_safe
     #sed -i "s@/usr/local/mysql@${mysql_install_dir}@g" ${mysql_install_dir}/bin/mysqld_safe
     
-    log info "配置启动文件"
+    # 配置启动文件
     cp ${mysql_install_dir}/support-files/mysql.server /etc/init.d/mysqld
     sed -i "s@^basedir=.*@basedir=${mysql_install_dir}@" /etc/init.d/mysqld
     sed -i "s@^datadir=.*@datadir=${mysql_data_dir}@" /etc/init.d/mysqld
     chmod +x /etc/init.d/mysqld
     chkconfig --add mysqld; chkconfig mysqld on;
     
-    log info "配置环境变量/etc/profile"
+    # 配置环境变量/etc/profile
     [ -z "$(grep ^'export PATH=' /etc/profile)" ] && echo "export PATH=${mysql_install_dir}/bin:\$PATH" >> /etc/profile
     [ -n "$(grep ^'export PATH=' /etc/profile)" -a -z "$(grep ${mysql_install_dir} /etc/profile)" ] && sed -i "s@^export PATH=\(.*\)@export PATH=${mysql_install_dir}/bin:\1@" /etc/profile
     . /etc/profile
 
-    log info "初始化数据库"
+    # 初始化数据库
     ${mysql_install_dir}/bin/mysqld --initialize-insecure --user=mysql --basedir=${mysql_install_dir} --datadir=${mysql_data_dir}
     service mysqld start
     
     ${mysql_install_dir}/bin/mysql -e "grant all privileges on *.* to root@'127.0.0.1' identified by \"${dbrootpwd}\" with grant option;"
     ${mysql_install_dir}/bin/mysql -e "grant all privileges on *.* to root@'localhost' identified by \"${dbrootpwd}\" with grant option;"
 
-    log info "创建hive的元数据库"
+    
+    # hive的元数据库
     ${mysql_install_dir}/bin/mysql -uroot -p${dbrootpwd} -e "create user 'hive'@'%' IDENTIFIED BY 'hive';GRANT ALL PRIVILEGES ON *.* TO 'hive'@'%' WITH GRANT OPTION;grant all on *.* to 'hive'@'localhost' identified by 'hive';flush privileges;"
 
     # 进行远程访问授权
