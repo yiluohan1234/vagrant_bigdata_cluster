3bc3ec495bcb6ecbf485f2e6b796e07fb6e5ce6d
yiluohan1234 <1097189275@qq.com>
2022-01-08 12:07:26 +0800

    update setup-mysql

--- scripts/setup-mysql.sh
+++ scripts/setup-mysql.sh
@@ -58,7 +58,7 @@ setup_mysql() {
 
     
     # hive的元数据库
-    ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "create user 'hive'@'%' IDENTIFIED BY 'hive';GRANT ALL PRIVILEGES ON *.* TO 'hive'@'%' WITH GRANT OPTION;grant all on *.* to 'hive'@'localhost' identified by 'hive';flush privileges;"
+    ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "create user hive@'%' IDENTIFIED BY 'hive';GRANT ALL PRIVILEGES ON *.* TO 'hive'@'%' WITH GRANT OPTION;grant all on *.* to 'hive'@'localhost' identified by 'hive';flush privileges;"
 
     # 进行远程访问授权
     ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "use mysql; update user set authentication_string=password(\"${MYSQL_PASSWORD}\") where user='root'; update user set authentication_string=password(\"${MYSQL_PASSWORD}\"),plugin='mysql_native_password' where user='root';grant all on *.* to root@'%' identified by \"${MYSQL_PASSWORD}\" with grant option;grant all privileges on *.* to 'root'@'%' identified by \"${MYSQL_PASSWORD}\" with grant option;flush privileges;"
@@ -70,16 +70,16 @@ setup_mysql() {
     ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE DATABASE maxwell;GRANT ALL ON maxwell.* TO 'maxwell'@'%' IDENTIFIED BY 'maxwell';GRANT SELECT, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO maxwell@'%';flush privileges;"
     
     # 在数据库建立azkaban数据库和用户
-    ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE DATABASE azkaban;CREATE USER 'azkaban'@'%' IDENTIFIED BY '199037';GRANT SELECT,INSERT,UPDATE,DELETE ON azkaban.* to 'azkaban'@'%' WITH GRANT OPTION;flush privileges;"
+    ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE DATABASE azkaban;CREATE USER azkaban@'%' IDENTIFIED BY '199037';GRANT SELECT,INSERT,UPDATE,DELETE ON azkaban.* to 'azkaban'@'%' WITH GRANT OPTION;flush privileges;"
     
     # 在数据库建立zabbix数据库和用户
-    ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE DATABASE zabbix character set utf8 collate utf8_bin;CREATE USER 'zabbix'@'%' IDENTIFIED BY '199037';GRANT SELECT,INSERT,UPDATE,DELETE ON zabbix.* to 'zabbix'@'%' WITH GRANT OPTION;flush privileges;"
+    ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE DATABASE zabbix character set utf8 collate utf8_bin;CREATE USER zabbix@'%' IDENTIFIED BY '$ZABBIX_DBPASSWORD';GRANT SELECT,INSERT,UPDATE,DELETE ON zabbix.* to 'zabbix'@'%' WITH GRANT OPTION;flush privileges;"
     
     # 在数据库建立ranger数据库和用户
-    ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE DATABASE ranger;CREATE USER 'ranger'@'%' IDENTIFIED BY 'ranger';GRANT all privileges ON ranger.* to 'ranger'@'%' identified by 'ranger';flush privileges;"
+    ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE DATABASE ranger;CREATE USER ranger@'%' IDENTIFIED BY 'ranger';GRANT all privileges ON ranger.* to 'ranger'@'%' identified by 'ranger';flush privileges;"
     
     # 创建数仓基本的数据库：gmall 和 gmall_report
-    ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE DATABASE gmall CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';;CREATE DATABASE gmall_report CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';"
+    ${mysql_install_dir}/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE DATABASE gmall CHARACTER SET utf8 COLLATE utf8_general_ci;CREATE DATABASE gmall_report CHARACTER SET utf8 COLLATE utf8_general_ci;"
     
 }
 
