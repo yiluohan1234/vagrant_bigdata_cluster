74dec568c0a5d6e32c2675fda5eb2cfacdecd266
yiluohan1234 <1097189275@qq.com>
2022-01-15 10:23:59 +0800

    Update setup-clean.sh

--- scripts/setup-clean.sh
+++ scripts/setup-clean.sh
@@ -2,8 +2,5 @@
 #set -x
 source "/vagrant/scripts/common.sh"
 
-# 设置安装目录权限
-chmod -R 660 $INSTALL_PATH
-chown -R $DEFAULT_USER:$DEFAULT_GROUP $INSTALL_PATH
 # 删除安装目录
 rm -rf ${INSTALL_PATH}/azkaban-3.84.4
