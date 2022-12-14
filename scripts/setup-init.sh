32dbb1fb1543f4da474fdb0a6dd88357afc18c29
yiluohan1234 <1097189275@qq.com>
2022-12-14 17:30:13 +0800

    Update setup-init.sh

--- scripts/setup-init.sh
+++ scripts/setup-init.sh
@@ -10,23 +10,29 @@ do
     # 各个用户的默认密码是vagrant
     echo $user | passwd --stdin $user
 done
+# 修改vagrant用户信息，把vagrant添加到组hadoop中
 usermod -a -G hadoop vagrant
 
 # 创建生成日志目录
-APP_LOG=/opt/module/applog/log/
-[ ! -d $INSTALL_PATH ] && mkdir -p $INSTALL_PATH
-[ ! -d $DOWNLOAD_PATH ] && mkdir -p $DOWNLOAD_PATH
-[ ! -d $INIT_SHELL_BIN ] && mkdir -p $INIT_SHELL_BIN
-[ ! -d $APP_LOG ] && mkdir -p $APP_LOG
-chown -R $DEFAULT_USER:$DEFAULT_GROUP $INSTALL_PATH
-chown -R $DEFAULT_USER:$DEFAULT_GROUP $DOWNLOAD_PATH
-chown -R $DEFAULT_USER:$DEFAULT_GROUP $INIT_SHELL_BIN
-chown -R $DEFAULT_USER:$DEFAULT_GROUP $APP_LOG
+# APP_LOG=/opt/module/applog/log/
+# [ ! -d $APP_LOG ] && mkdir -p $APP_LOG
+# chown -R ${DEFAULT_USER}:${DEFAULT_GROUP} $APP_LOG
+[ ! -d ${INSTALL_PATH} ] && mkdir -p ${INSTALL_PATH}
+[ ! -d ${DOWNLOAD_PATH} ] && mkdir -p ${DOWNLOAD_PATH}
+[ ! -d ${INIT_SHELL_BIN} ] && mkdir -p ${INIT_SHELL_BIN}
+chown -R ${DEFAULT_USER}:$DEFAULT_GROUP ${INSTALL_PATH}
+chown -R ${DEFAULT_USER}:${DEFAULT_GROUP} ${DOWNLOAD_PATH}
+chown -R ${DEFAULT_USER}:${DEFAULT_GROUP} ${INIT_SHELL_BIN}
+
 
 # 复制初始化程序到init_shell的bin目录
 log info "copy init shell to ${INIT_SHELL_BIN}"
+for name in ${HOSTNAME_LIST[@]}; do host_list="${host_list:-} ""$name"; done
 if [ ${INSTALL_PATH} != /home/vagrant/apps ];then
     sed -i "s@/home/vagrant/apps@${INSTALL_PATH}@g" `grep '/home/vagrant/apps' -rl ${INIT_PATH}/`
+    sed -i "s@hdp{101..103}@${host_list}@g"  ${INIT_PATH}/xsync
+    sed -i "s@hdp{101..103}@${host_list}@g"  ${INIT_PATH}/xcall
+    sed -i "s@hdp{101..103}@${host_list}@g"  ${INIT_PATH}/jpsall
 fi
 cp $INIT_PATH/jpsall ${INIT_SHELL_BIN}
 cp $INIT_PATH/bigstart ${INIT_SHELL_BIN}
@@ -44,19 +50,15 @@ echo 'export PATH=${INIT_SHELL_BIN}:$PATH' >> ${PROFILE}
 source ${PROFILE}
 
 # 设置安装目录权限
-chmod -R 755 $INSTALL_PATH
-chown -R $DEFAULT_USER:$DEFAULT_GROUP $INSTALL_PATH
+chmod -R 755 ${INSTALL_PATH}
+chown -R ${DEFAULT_USER}:${DEFAULT_GROUP} ${INSTALL_PATH}
 
 # 生成免密登录
 log info "生成免密登录"
 setssh
-su - hdfs -s /bin/bash setssh
-su - yarn -s /bin/bash setssh
-su - mapred -s /bin/bash setssh
-su - hive -s /bin/bash setssh
-su - vagrant -s /bin/bash setssh
 
 # 统一缩进为4
-echo "set tabstop=4" >> $PROFILE
-echo "set softtabstop=4" >> $PROFILE
-echo "set shiftwidth=4" >> $PROFILE
+echo "set tabstop=4" >> ${PROFILE}
+echo "set softtabstop=4" >> ${PROFILE}
+echo "set shiftwidth=4" >> ${PROFILE}
+echo -e "\n" >> ${PROFILE}
