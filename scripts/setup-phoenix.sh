f22c826c31077699c570a9b859a6eb388bf08d3a
yiluohan1234 <1097189275@qq.com>
2022-12-15 19:41:44 +0800

    Update phoenix

--- scripts/setup-phoenix.sh
+++ scripts/setup-phoenix.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #set -x
-source "/vagrant/scripts/common.sh"
+if [ -d /vagrant/scripts ];then
+    source "/vagrant/scripts/common.sh"
+fi
 
 setup_phoenix() {
     local app_name=$1
@@ -11,44 +13,32 @@ setup_phoenix() {
     # cp ${INSTALL_PATH}/phoenix/phoenix-core-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}.jar ${INSTALL_PATH}/hbase/lib
     cp -f ${HBASE_RES_DIR}/hbase-site.xml ${INSTALL_PATH}/phoenix/bin
     if [ "${IS_VAGRANT}" != "true" ];then
-        for i in {"hdp102","hdp103"};
+        length=${#HOSTNAME_LIST[@]}
+        for ((i=0; i<$length; i++))
         do
-            scp ${INSTALL_PATH}/phoenix/phoenix-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}-server.jar vagrant@$i:${INSTALL_PATH}/hbase/lib
-            # scp ${INSTALL_PATH}/phoenix/phoenix-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}-client.jar vagrant@$i:${INSTALL_PATH}/hbase/lib
-            # scp ${INSTALL_PATH}/phoenix/phoenix-core-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}.jar vagrant@$i:${INSTALL_PATH}/hbase/lib
+            host=${HOSTNAME_LIST[1]}
+            current_hostname=`cat /etc/hostname`
+            if [ "$current_hostname" != "$host" ];then
+                scp ${INSTALL_PATH}/phoenix/phoenix-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}-server.jar ${DEFAULT_USER}@$host:${INSTALL_PATH}/hbase/lib
+                # scp ${INSTALL_PATH}/phoenix/phoenix-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}-client.jar vagrant@$i:${INSTALL_PATH}/hbase/lib
+                # scp ${INSTALL_PATH}/phoenix/phoenix-core-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}.jar vagrant@$i:${INSTALL_PATH}/hbase/lib
+            fi
         done
     fi
 }
 
-download_phoenix() {
-    local app_name=$1
-    local app_name_upper=$(echo $app_name | tr '[a-z]' '[A-Z]')
-    local app_version=$(eval echo \$${app_name_upper}_VERSION)
-    local archive=$(eval echo \$${app_name_upper}_ARCHIVE)
-    local download_url=$(eval echo \$${app_name_upper}_MIRROR_DOWNLOAD)
-
-    log info "install $app_name"
-    if resourceExists $archive; then
-        installFromLocal $archive
-    else
-        installFromRemote $archive $download_url
-    fi
-    mv ${INSTALL_PATH}/"${app_version}" ${INSTALL_PATH}/${app_name}
-    chown -R $DEFAULT_USER:$DEFAULT_GROUP ${INSTALL_PATH}/${app_name}
-    rm $DOWNLOAD_PATH/${archive}
-}
-
 install_phoenix() {
-    log info "setup phoenix"
     app_name="phoenix"
-
-    download_phoenix $app_name
-    setup_phoenix $app_name
-    setupEnv_app $app_name
-    if [ "$IS_VAGRANT" != "true" ];then
-        dispatch_app ${app_name}
+    log info "setup phoenix"
+    if [ ! -d ${INSTALL_PATH}/${app_name} ];then
+        download_and_unzip_app $app_name
+        setup_phoenix $app_name
+        setupEnv_app $app_name
+        if [ "$IS_VAGRANT" != "true" ];then
+            dispatch_app ${app_name}
+        fi
+        source $PROFILE
     fi
-    source $PROFILE
 }
 
 if [ "$IS_VAGRANT" == "true" ];then
