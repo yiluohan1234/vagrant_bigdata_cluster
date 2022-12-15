87912b31790a0edba95aa163b73d120aba2dfe4b
yiluohan1234 <1097189275@qq.com>
2022-12-15 17:57:27 +0800

    Update spark

--- scripts/setup-spark.sh
+++ scripts/setup-spark.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #set -x
-source "/vagrant/scripts/common.sh"
+if [ -d /vagrant/scripts ];then
+    source "/vagrant/scripts/common.sh"
+fi
 
 setup_spark() {
     local app_name=$1
@@ -13,7 +15,7 @@ setup_spark() {
     cp -f ${res_dir}/slaves ${conf_dir}
     cp -f ${res_dir}/spark-defaults.conf ${conf_dir}
     cp -f ${res_dir}/spark-env.sh ${conf_dir}
-    wget_mysql_connector ${INSTALL_PATH}/spark/jars
+    wget_mysql_connector ${INSTALL_PATH}/${app_name}/jars
 
     if [ ${INSTALL_PATH} != /home/vagrant/apps ];then
         sed -i "s@/home/vagrant/apps@${INSTALL_PATH}@g" `grep '/home/vagrant/apps' -rl ${conf_dir}/`
@@ -26,35 +28,16 @@ setup_spark() {
     #cp -rf ${INSTALL_PATH}/spark/jars/*.jar ${INSTALL_PATH}/hive/lib/
 }
 
-download_spark() {
-    local app_name=$1
-    local app_name_upper=`get_string_upper ${app_name}`
-    local app_version=$(eval echo \$${app_name_upper}_VERSION)
-    local archive=$(eval echo \$${app_name_upper}_ARCHIVE)
-    local download_url=$(eval echo \$${app_name_upper}_MIRROR_DOWNLOAD)
-
-    log info "install ${app_name}"
-    if resourceExists ${archive}; then
-        installFromLocal ${archive}
-    else
-        installFromRemote ${archive} ${download_url}
-    fi
-    mv ${INSTALL_PATH}/"${SPARK_VERSION}-bin-hadoop3.2" ${INSTALL_PATH}/${app_name}
-    chown -R $DEFAULT_USER:$DEFAULT_GROUP ${INSTALL_PATH}/${app_name}
-    rm ${DOWNLOAD_PATH}/${archive}
-}
-
 install_spark() {
     local app_name="spark"
     if [ ! -d ${INSTALL_PATH}/${app_name} ];then
         log info "setup ${app_name}"
-
-        download_spark ${app_name}
+        download_and_unzip_app ${app_name}
         setup_spark ${app_name}
         setupEnv_app ${app_name}
-        # if [ "${IS_VAGRANT}" != "true" ];then
-        #     dispatch_app ${app_name}
-        # fi
+        if [ "${IS_VAGRANT}" != "true" ];then
+            dispatch_app ${app_name}
+        fi
         source ${PROFILE}
     fi
 }
