0a8441cc57a5b0d40ae2444df34336ea1c4d9034
yiluohan1234 <1097189275@qq.com>
2022-12-15 19:13:05 +0800

    Update Maxwell

--- scripts/setup-maxwell.sh
+++ scripts/setup-maxwell.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #set -x
-source "/vagrant/scripts/common.sh"
-
+if [ -d /vagrant/scripts ];then
+    source "/vagrant/scripts/common.sh"
+fi
 
 setup_maxwell() {
     local app_name=$1
@@ -16,29 +17,10 @@ setup_maxwell() {
     #${mysql_install_dir}/bin/mysql -uroot -p${dbrootpwd} -e "CREATE DATABASE maxwell;GRANT ALL ON maxwell.* TO 'maxwell'@'%' IDENTIFIED BY 'maxwell';GRANT SELECT, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO maxwell@'%';flush privileges;"
 }
 
-download_maxwell() {
-    local app_name=$1
-    local app_name_upper=`get_string_upper ${app_name}`
-    local app_version=$(eval echo \$${app_name_upper}_VERSION)
-    local archive=$(eval echo \$${app_name_upper}_ARCHIVE)
-    local download_url=$(eval echo \$${app_name_upper}_MIRROR_DOWNLOAD)
-
-    log info "install ${app_name}"
-    if resourceExists ${archive}; then
-        installFromLocal ${archive}
-    else
-        installFromRemote ${archive} ${download_url}
-    fi
-    mv ${INSTALL_PATH}/"${app_version}" ${INSTALL_PATH}/${app_name}
-    chown -R $DEFAULT_USER:$DEFAULT_GROUP ${INSTALL_PATH}/${app_name}
-    rm ${DOWNLOAD_PATH}/${archive}
-}
-
 install_maxwell() {
     local app_name="maxwell"
     log info "setup ${app_name}"
-
-    download_maxwell ${app_name}
+    download_and_unzip_app ${app_name}
     setup_maxwell ${app_name}
     setupEnv_app ${app_name}
     source ${PROFILE}
