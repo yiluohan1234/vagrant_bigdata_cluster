617611d5d7fe67a52ee611daed1bc8f279b685b2
yiluohan1234 <1097189275@qq.com>
2022-12-15 19:05:58 +0800

    Update Kylin

--- scripts/setup-kylin.sh
+++ scripts/setup-kylin.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #set -x
-source "/vagrant/scripts/common.sh"
+if [ -d /vagrant/scripts ];then
+    source "/vagrant/scripts/common.sh"
+fi
 
 setup_kylin() {
     local app_name=$1
@@ -17,35 +19,16 @@ setup_kylin() {
     fi
 }
 
-download_kylin() {
-    local app_name=$1
-    local app_name_upper=`get_string_upper ${app_name}`
-    local app_version=$(eval echo \$${app_name_upper}_VERSION)
-    local archive=$(eval echo \$${app_name_upper}_ARCHIVE)
-    local download_url=$(eval echo \$${app_name_upper}_MIRROR_DOWNLOAD)
-
-    log info "install ${app_name}"
-    if resourceExists ${archive}; then
-        installFromLocal ${archive}
-    else
-        installFromRemote ${archive} ${download_url}
-    fi
-    mv ${INSTALL_PATH}/"${app_version}-bin-hadoop3" ${INSTALL_PATH}/${app_name}
-    chown -R $DEFAULT_USER:$DEFAULT_GROUP ${INSTALL_PATH}/${app_name}
-    rm ${DOWNLOAD_PATH}/${archive}
-}
-
 install_kylin() {
     local app_name="kylin"
     log info "setup ${app_name}"
     if [ ! -d ${INSTALL_PATH}/${app_name} ];then
-        download_kylin ${app_name}
+        download_and_unzip_app ${app_name}
         setup_kylin ${app_name}
         setupEnv_app $app_name
         source ${PROFILE}
     fi
 }
-
 
 if [ "${IS_VAGRANT}" == "true" ];then
     install_kylin
