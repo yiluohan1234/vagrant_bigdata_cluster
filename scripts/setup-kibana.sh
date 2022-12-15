0a3beed44a2876ffc69e867d21643297b86b33fc
yiluohan1234 <1097189275@qq.com>
2022-12-15 19:20:59 +0800

    Update kibana

--- scripts/setup-kibana.sh
+++ scripts/setup-kibana.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #set -x
-source "/vagrant/scripts/common.sh"
+if [ -d /vagrant/scripts ];then
+    source "/vagrant/scripts/common.sh"
+fi
 
 setup_kibana() {
     local app_name=$1
@@ -10,36 +12,14 @@ setup_kibana() {
 
     log info "copying over $app_name configuration files"
     cp -f ${res_dir}/* ${conf_dir}
-    mkdir -p ${INSTALL_PATH}/kibana/logs
+    mkdir -p ${INSTALL_PATH}/${app_name}/logs
 
-    if [ ${INSTALL_PATH} != /home/vagrant/apps ];then
-        sed -i "s@/home/vagrant/apps@${INSTALL_PATH}@g" `grep '/home/vagrant/apps' -rl ${conf_dir}/`
-    fi
-}
-
-download_kibana() {
-    local app_name=$1
-    local app_name_upper=`get_string_upper ${app_name}`
-    local app_version=$(eval echo \$${app_name_upper}_VERSION)
-    local archive=$(eval echo \$${app_name_upper}_ARCHIVE)
-    local download_url=$(eval echo \$${app_name_upper}_MIRROR_DOWNLOAD)
-
-    log info "install ${app_name}"
-    if resourceExists ${archive}; then
-        installFromLocal ${archive}
-    else
-        installFromRemote ${archive} ${download_url}
-    fi
-    mv ${INSTALL_PATH}/${KIBANA_VERSION}-linux-x86_64 ${INSTALL_PATH}/${app_name}
-    chown -R $DEFAULT_USER:$DEFAULT_GROUP ${INSTALL_PATH}/${app_name}
-    rm ${DOWNLOAD_PATH}/${archive}
 }
 
 install_kibana() {
     local app_name="kibana"
     log info "setup ${app_name}"
-
-    download_kibana ${app_name}
+    download_and_unzip_app ${app_name}
     setup_kibana ${app_name}
     setupEnv_app ${app_name}
     source ${PROFILE}
