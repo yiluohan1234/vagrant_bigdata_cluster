fb4fd18e4a32477aef358a113ccdb12afb1bec66
yiluohan1234 <1097189275@qq.com>
2022-12-15 17:19:21 +0800

    Update scala

--- scripts/setup-scala.sh
+++ scripts/setup-scala.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
-source "/vagrant/scripts/common.sh"
-
-download_scala() {
-    local app_name=$1
-    local app_name_upper=`get_string_upper ${app_name}`
-    local app_version=$(eval echo \$${app_name_upper}_VERSION)
-    local archive=$(eval echo \$${app_name_upper}_ARCHIVE)
-    local download_url=$(eval echo \$${app_name_upper}_MIRROR_DOWNLOAD)
-
-    log info "install ${app_name}"
-    if resourceExists ${archive}; then
-        installFromLocal ${archive}
-    else
-        installFromRemote ${archive} ${download_url}
-    fi
-    mv ${INSTALL_PATH}/"${app_version}" ${INSTALL_PATH}/${app_name}
-    chown -R $DEFAULT_USER:$DEFAULT_GROUP ${INSTALL_PATH}/${app_name}
-    rm ${DOWNLOAD_PATH}/${archive}
-}
+if [ -d /vagrant/scripts ];then
+    source "/vagrant/scripts/common.sh"
+fi 
 
 install_scala() {
     local app_name="scala"
     if [ ! -d ${INSTALL_PATH}/${app_name} ];then
         log info "setup ${app_name}"
-        download_scala ${app_name}
+        download_and_unzip_app ${app_name}
         setupEnv_app ${app_name}
         if [ "${IS_VAGRANT}" != "true" ];then
             dispatch_app ${app_name}
