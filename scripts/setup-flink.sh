45dee593cc04f4a1b2a924422991d4d1a15fb20a
yiluohan1234 <1097189275@qq.com>
2022-12-17 22:35:11 +0800

    Update flink

--- scripts/setup-flink.sh
+++ scripts/setup-flink.sh
@@ -10,12 +10,29 @@ setup_flink() {
     local res_dir=$(eval echo \$${app_name_upper}_RES_DIR)
     local conf_dir=$(eval echo \$${app_name_upper}_CONF_DIR)
 
-    log info "copying over ${app_name} configuration files"
-    cp -f ${res_dir}/* ${conf_dir}
+    log info "modifying over ${app_name} configuration files"
+    # cp -f ${res_dir}/* ${conf_dir}
 
-    if [ ${INSTALL_PATH} != /home/vagrant/apps ];then
-        sed -i "s@/home/vagrant/apps@${INSTALL_PATH}@g" `grep '/home/vagrant/apps' -rl ${conf_dir}/`
-    fi
+    # flink-conf.yaml
+    echo "jobmanager.rpc.address: ${HOSTNAME_LIST[0]}" >> ${conf_dir}/flink-conf.yaml
+    echo "jobmanager.rpc.port: 6123" >> ${conf_dir}/flink-conf.yaml
+    echo "jobmanager.heap.size: 1024m" >> ${conf_dir}/flink-conf.yaml
+    echo "taskmanager.heap.size: 1024m" >> ${conf_dir}/flink-conf.yaml
+    echo "taskmanager.numberOfTaskSlots: 10" >> ${conf_dir}/flink-conf.yaml
+    echo "taskmanager.memory.preallocate: false" >> ${conf_dir}/flink-conf.yaml
+    echo "parallelism.default: 1" >> ${conf_dir}/flink-conf.yaml
+    echo "jobmanager.web.port: 8381" >> ${conf_dir}/flink-conf.yaml
+    echo "rest.port: 8381" >> ${conf_dir}/flink-conf.yaml
+    echo "env.java.opts: -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -XX:+AlwaysPreTouch -server -XX:+HeapDumpOnOutOfMemoryError" >> ${conf_dir}/flink-conf.yaml
+    echo "env.java.home: ${INSTALL_PATH}/java" >> ${conf_dir}/flink-conf.yaml
+    echo "classloader.resolve-order: parent-first" >> ${conf_dir}/flink-conf.yaml
+
+    # masters and workers
+    sed -i '1,$d' ${conf_dir}/masters 
+    echo "${HOSTNAME_LIST[0]}" >> ${conf_dir}/masters
+    sed -i '1,$d' ${conf_dir}/regionservers 
+    echo -e "${HOSTNAME_LIST[0]}\n${HOSTNAME_LIST[1]}\n${HOSTNAME_LIST[2]}" >> ${conf_dir}/workers
+
 }
 
 install_flink() {
