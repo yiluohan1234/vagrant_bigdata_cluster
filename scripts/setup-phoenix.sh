1e4a23560793c072c7e0fbb5eedc901a5f388f71
yiluohan1234 <1097189275@qq.com>
2022-12-17 22:34:53 +0800

    Update phoenix

--- scripts/setup-phoenix.sh
+++ scripts/setup-phoenix.sh
@@ -8,23 +8,26 @@ setup_phoenix() {
     local app_name=$1
 
     log info "copying server.jar to hbase"
-    cp ${INSTALL_PATH}/phoenix/phoenix-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}-server.jar ${INSTALL_PATH}/hbase/lib
+    cp ${INSTALL_PATH}/phoenix/phoenix-server-hbase-${HBASE_VERSION_NUM}-${PHOENIX_VERSION_NUM}.jar ${INSTALL_PATH}/hbase/lib
     # cp ${INSTALL_PATH}/phoenix/phoenix-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}-client.jar ${INSTALL_PATH}/hbase/lib
     # cp ${INSTALL_PATH}/phoenix/phoenix-core-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}.jar ${INSTALL_PATH}/hbase/lib
-    cp -f ${HBASE_RES_DIR}/hbase-site.xml ${INSTALL_PATH}/phoenix/bin
-    if [ "${IS_VAGRANT}" != "true" ];then
-        length=${#HOSTNAME_LIST[@]}
-        for ((i=0; i<$length; i++))
-        do
-            host=${HOSTNAME_LIST[0]}
-            current_hostname=`cat /etc/hostname`
-            if [ "$current_hostname" != "$host" ];then
-                scp ${INSTALL_PATH}/phoenix/phoenix-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}-server.jar ${DEFAULT_USER}@$host:${INSTALL_PATH}/hbase/lib
-                # scp ${INSTALL_PATH}/phoenix/phoenix-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}-client.jar vagrant@$i:${INSTALL_PATH}/hbase/lib
-                # scp ${INSTALL_PATH}/phoenix/phoenix-core-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}.jar vagrant@$i:${INSTALL_PATH}/hbase/lib
-            fi
-        done
-    fi
+    cp ${INSTALL_PATH}/hbase/conf/hbase-site.xml ${INSTALL_PATH}/phoenix/bin
+}
+
+dispatch_phoenix() {
+    local app_name=$1
+    log info "dispatch ${app_name}" 
+    dispatch_app ${app_name}
+
+    length=${#HOSTNAME_LIST[@]}
+    for ((i=0; i<$length; i++));do
+        current_hostname=`cat /etc/hostname`
+        if [ "$current_hostname" != "${HOSTNAME_LIST[$i]}" ];then
+            scp ${INSTALL_PATH}/phoenix/phoenix-server-hbase-${HBASE_VERSION_NUM}-${PHOENIX_VERSION_NUM}.jar ${DEFAULT_USER}@${HOSTNAME_LIST[$i]}:${INSTALL_PATH}/hbase/lib
+            # scp ${INSTALL_PATH}/phoenix/phoenix-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}-client.jar vagrant@$i:${INSTALL_PATH}/hbase/lib
+            # scp ${INSTALL_PATH}/phoenix/phoenix-core-${PHOENIX_VERSION_NUM}-HBase-${HBASE_VERSION_NUM:0:3}.jar vagrant@$i:${INSTALL_PATH}/hbase/lib
+        fi
+    done
 }
 
 install_phoenix() {
@@ -35,7 +38,7 @@ install_phoenix() {
         setup_phoenix $app_name
         setupEnv_app $app_name
         if [ "$IS_VAGRANT" != "true" ];then
-            dispatch_app ${app_name}
+            dispatch_phoenix ${app_name}
         fi
         source $PROFILE
     fi
