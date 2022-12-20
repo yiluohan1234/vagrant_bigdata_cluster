eeb2ff292f1375a52a827518e1506faef01d3ec6
yiluohan1234 <1097189275@qq.com>
2022-12-20 14:22:00 +0800

    Update clean

--- scripts/setup-clean.sh
+++ scripts/setup-clean.sh
@@ -4,6 +4,8 @@ if [ -d /vagrant/scripts ];then
     source "/vagrant/scripts/common.sh"
 fi
 
+# 更新安装目录的用户
+chown -R $DEFAULT_USER:$DEFAULT_GROUP ${INSTALL_PATH}
 # 删除安装目录
 [ -d ${INSTALL_PATH}/azkaban-3.84.4 ] && rm -rf ${INSTALL_PATH}/azkaban-3.84.4
 [ -f /root/anaconda-ks.cfg ] && rm -rf /root/anaconda-ks.cfg  
