27defbdf564e790ae37619e284d9779d6f8900f6
yiluohan1234 <1097189275@qq.com>
2023-07-27 15:36:56 +0800

    update f1-3

--- resources/init_bin/shop5/f1.sh
+++ resources/init_bin/shop5/f1.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 case $1 in
-"start"){
+    start)
         for i in hdp101 hdp102
         do
-                echo " --------启动 $i 采集flume-------"
-                ssh $i "source /etc/profile; nohup /opt/module/flume/bin/flume-ng agent -n a1 -c /opt/module/flume/conf/ -f /opt/module/flume/job/file_to_kafka.conf >/dev/null 2>&1 &"
+            echo " --------启动 $i 采集flume-------"
+            ssh $i "source /etc/profile; nohup /opt/module/flume/bin/flume-ng agent -n a1 -c /opt/module/flume/conf/ -f /opt/module/flume/job/file_to_kafka.conf >/dev/null 2>&1 &"
         done
-};;
-"stop"){
+        ;;
+    stop)
         for i in hdp101 hdp102
         do
-                echo " --------停止 $i 采集flume-------"
-                ssh $i "ps -ef | grep file_to_kafka | grep -v grep |awk  '{print \$2}' | xargs -n1 kill -9 "
+            echo " --------停止 $i 采集flume-------"
+            ssh $i "ps -ef | grep file_to_kafka | grep -v grep |awk  '{print \$2}' | xargs -n1 kill -9 "
         done
-
-};;
+        ;;
 esac
