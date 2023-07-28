888b59bb19317b48ab0651633ebfee3f91d825da
yiluohan1234 <1097189275@qq.com>
2023-07-28 09:14:36 +0800

    update dataware

---
+++ scripts/setup-dataware.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#set -x
+if [ -d /vagrant/scripts ];then
+    source "/vagrant/scripts/common.sh"
+fi
+
+install_dataware() {
+    local app_name="dataware"
+    log info "setup ${app_name}"
+    mkdir -p ${INSTALL_PATH}/dataware
+
+    cp $INIT_PATH/${DATAWARE_VERSION}/* ${INIT_SHELL_BIN}
+
+    git clone https://gitee.com/yiluohan1234/vagrant_bigdata ${INSTALL_PATH}/tmp
+    mv ${INSTALL_PATH}/tmp/dataware/${DATAWARE_VERSION}/mock/* ${INSTALL_PATH}/dataware
+    mv ${INSTALL_PATH}/tmp/dataware/${DATAWARE_VERSION}/flume/*.jar ${INSTALL_PATH}/flume/lib
+    #rm -rf ${INSTALL_PATH}/tmp
+
+}
+if [ "${IS_VAGRANT}" == "true" ];then
+    install_dataware
+fi
