c22c0410bd214f28f196d24954d9582d921bb382
yiluohan1234 <1097189275@qq.com>
2023-07-28 16:47:23 +0800

    update maxwell conf

--- scripts/setup-maxwell.sh
+++ scripts/setup-maxwell.sh
@@ -17,7 +17,7 @@ setup_maxwell() {
     sed -i "s@^host=.*@host=${HOSTNAME_LIST[2]}@g" ${conf_dir}/config.properties
     sed -i "11a# kafka topic"  ${conf_dir}/config.properties
     sed -i "/# kafka topic/a kafka_topic=topic_db"  ${conf_dir}/config.properties
-    sed -i "/password=maxwell/a jdbc_options=useSSL=false&serverTimezone=Asia/Shanghai"  ${conf_dir}/config.properties
+    sed -i "/^password=maxwell/a jdbc_options=useSSL=false&serverTimezone=Asia/Shanghai"  ${conf_dir}/config.properties
 
     # Delete guava-11.0.2.jar in the lib folder to be compatible with Hadoop-3.1.3
     rm ${INSTALL_PATH}/${app_name}/lib/guava-*.jar
