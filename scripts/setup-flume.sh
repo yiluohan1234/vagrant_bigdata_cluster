91026cfebd0a6a4bfe5c44d5337d47fb3f9c9377
yiluohan1234 <1097189275@qq.com>
2023-07-31 15:47:35 +0800

    Update flume

--- scripts/setup-flume.sh
+++ scripts/setup-flume.sh
@@ -14,22 +14,22 @@ setup_flume() {
     # flume-env.sh
     cp ${conf_dir}/flume-env.sh.template ${conf_dir}/flume-env.sh
     sed -i "s@^# export JAVA_HOME=.*@export JAVA_HOME=${INSTALL_PATH}/java@" ${conf_dir}/flume-env.sh
-    sed -i 's@^# export JAVA_OPTS=.*@export JAVA_OPTS="-Xms100m -Xmx2000m -Dcom.sun.management.jmxremote"@' ${conf_dir}/flume-env.sh
+    sed -i 's@^# export JAVA_OPTS=".-*@export JAVA_OPTS="-Xms100m -Xmx2000m -Dcom.sun.management.jmxremote"@' ${conf_dir}/flume-env.sh
     # flume-conf.properties
     cp ${INSTALL_PATH}/${app_name}/conf/flume-conf.properties.template ${INSTALL_PATH}/${app_name}/conf/flume-conf.properties
 
     # Delete guava-11.0.2.jar in the lib folder to be compatible with Hadoop-3.1.3
     rm ${INSTALL_PATH}/${app_name}/lib/guava-*.jar
 
-    log info "copying $app_name configuration files"
-    mkdir -p ${INSTALL_PATH}/${app_name}/job
-    cp ${res_dir}/${DATAWARE_VERSION}/*.conf ${INSTALL_PATH}/${app_name}/job
+    log4j_path=${conf_dir}/log4j.properties
+    log_path=${INSTALL_PATH}/${app_name}/logs
+    sed -i 's@^flume.log.dir=.*@flume.log.dir='${log_path}'@' ${log4j_path}
 
 
     # Replace the default host configuration
-    sed -i "s@hdp101@${HOSTNAME_LIST[0]}@g" `grep 'hdp101' -rl ${conf_dir}/`
-    sed -i "s@hdp102@${HOSTNAME_LIST[1]}@g" `grep 'hdp102' -rl ${conf_dir}/`
-    sed -i "s@hdp103@${HOSTNAME_LIST[2]}@g" `grep 'hdp103' -rl ${conf_dir}/`
+    # sed -i "s@hdp101@${HOSTNAME_LIST[0]}@g" `grep 'hdp101' -rl ${conf_dir}/`
+    # sed -i "s@hdp102@${HOSTNAME_LIST[1]}@g" `grep 'hdp102' -rl ${conf_dir}/`
+    # sed -i "s@hdp103@${HOSTNAME_LIST[2]}@g" `grep 'hdp103' -rl ${conf_dir}/`
 
     # if [ "${IS_KERBEROS}" != "true" ];then
     #     sed -i '39,40d' ${conf_dir}/kafka-flume-hdfs.conf
