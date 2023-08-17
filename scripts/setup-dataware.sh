9bb6a61aa599ff6e118df66f6bc0b5d80a4da1d4
yiluohan1234 <1097189275@qq.com>
2023-08-17 21:18:05 +0800

    init

--- scripts/setup-dataware.sh
+++ scripts/setup-dataware.sh
@@ -20,6 +20,8 @@ install_dataware5() {
 
     git clone https://gitee.com/yiluohan1234/vagrant_bigdata ${INSTALL_PATH}/tmp
     mv ${INSTALL_PATH}/tmp/dataware/${DATAWARE_VERSION}/mock/* ${INSTALL_PATH}/dataware
+    mv ${INSTALL_PATH}/tmp/dataware/${DATAWARE_VERSION}/sbin/ ${INSTALL_PATH}/dataware
+    mv ${INSTALL_PATH}/tmp/dataware/${DATAWARE_VERSION}/sql/ ${INSTALL_PATH}/dataware
     mv ${INSTALL_PATH}/tmp/dataware/${DATAWARE_VERSION}/flume/*.jar ${INSTALL_PATH}/flume/lib
     # flume conf
     mkdir -p ${INSTALL_PATH}/flume/job
