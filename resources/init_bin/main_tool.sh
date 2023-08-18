ec9ec5fc1b652e935a7f9654d0e1d0fb21f44412
yiluohan1234 <1097189275@qq.com>
2023-08-18 20:30:18 +0800

    add data mock

--- resources/init_bin/main_tool.sh
+++ resources/init_bin/main_tool.sh
@@ -6,7 +6,7 @@ _main_tool()
     local cmd=${COMP_WORDS[COMP_CWORD-1]}
     case $cmd in
         'main')
-        COMPREPLY=( $(compgen -W 'f1 f2 f3 lg db lg_init db_init init cluster' -- $cur ) )
+        COMPREPLY=( $(compgen -W 'f1 f2 f3 lg db lg_init db_init init cluster mock' -- $cur ) )
         ;;
 
         'f1')
