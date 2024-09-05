275e8cf0ea00be6ed5bf4bee1134288568b83a6b
luojiahan <cuiyufei1234@126.com>
2024-09-05 16:13:05 +0800

    del rpm file

--- resources/bdcompetition/cluster.sh
+++ resources/bdcompetition/cluster.sh
@@ -450,6 +450,7 @@ setssh(){
         curl -o /root/tcl-8.5.13-8.el7.x86_64.rpm -O -L https://gitee.com/yiluohan1234/vagrant_bigdata_cluster/raw/master/resources/bdcompetition/other/tcl-8.5.13-8.el7.x86_64.rpm
         yum install -y -q /root/tcl-8.5.13-8.el7.x86_64.rpm
         yum install -y -q /root/expect-5.45-14.el7_1.x86_64.rpm
+        rm -rf /root/*.rpm
     fi
     if [ ! -f ~/.ssh/id_rsa ];then
         expect -c "
